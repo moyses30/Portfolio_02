56cda32c5a43bcd0441a9d7125d3639611d57520
Moyses Josef <moysesjosef@gmail.com>
2024-11-25 21:37:09 -0300

    novo

---
+++ envia.php
@@ -0,0 +1,23 @@
+<?php
+    $nome = addslashes ($_POST['nome']);
+    $email = addslashes ($_POST['email']);
+    $telefone = addslashes ($_POST['telefone']);
+    $mensagem = addslashes ($_POST['mensagem']) ;
+
+    $para = "moysesjosef@gmail.com"  ;
+    $assunto = "coleta de dados- Portfolio "
+
+    $corpo = "Nome :". $nome."\n"."email".$email."\n"
+
+    $cabeca= "from : moysesvalgore@gmail.com"."\n"."Reply-to:".$email."\n"."X=Mailer:PHP/".phpversion();
+
+   if (mail($para,$assunto,$corpo,$cabeca)) {
+    echo("Email enviado com sucesso!");
+    
+}else{
+    echo(" Houve um erro ao enviar o email");
+}
+
+
+
+?>
